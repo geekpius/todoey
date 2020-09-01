7253dec9c0a319ab16ad5f67bf78529d7dd22ebb
Fiifi Pius <fiifipius@gmail.com>
2020-09-01 19:45:56 +0000

    check box refactor

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:todoey/widgets/task_checkbox.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task.'),
-      trailing: Checkbox(
-        value: false,
+      title: Text(
+        'This is a task.',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: TaskCheckBox(
+        checkboxState: isChecked,
+        toggleCheckBoxState: (newCheckBoxState) {
+          setState(() {
+            isChecked = newCheckBoxState;
+          });
+        },
       ),
     );
   }
 }
+
